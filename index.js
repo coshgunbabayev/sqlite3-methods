7d3dc7800f5cab3a6fe84b060cd405f2e39c257d
coshgunbabayev <coshgunbabayevw13@gmail.com>
2024-06-16 00:52:30 +0400

    v0.0.4

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const sqlite3 = require("sqlite3").verbose(); let sqlcommand;
 const { OPEN_READWRITE } = require("sqlite3");
-const db = new sqlite3.Database("./database/B.db", sqlite3 / OPEN_READWRITE, (err) => { if (err) return console.error(err.message); });
 
-const connection = (file) => {
-    const db = new sqlite3.Database(file,
+const connection = async (file) => {
+    const db = await new sqlite3.Database(file,
         sqlite3 / OPEN_READWRITE,
         (err) => { if (err) return new Error("Error: file is not a database") });
     return db
